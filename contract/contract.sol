e673bee288160c260503ebea78112e1f92fa8526
AftAb-25 <aftab123215@gmail.com>
2025-11-28 13:34:38 +0530

    update contract.sol

--- contract/contract.sol
+++ contract/contract.sol
@@ -1,49 +1,24 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity ^0.8.0;
+// License (required)
+ // SPDX-License-Identifier: MIT
 
-// contract MultiWill {
-//     struct Will {
-//         address recipient;
-//         uint256 amount;
-//         bool claimed;
-//     }
+// Solidity version
+pragma solidity ^0.8.0;
 
-//     mapping(address => Will[]) public wills; // Each owner can have multiple wills
+// Smart Contract name
+contract MoodTracker {
 
-//     function createWill(address _recipient) public payable {
-//         require(_recipient != address(0), "Invalid recipient address");
-//         require(msg.value > 0, "Amount must be greater than zero");
+    // This variable stores the mood as text
+    string public mood;
 
-//         wills[msg.sender].push(Will({
-//             recipient: _recipient,
-//             amount: msg.value,
-//             claimed: false
-//         }));
-//     }
+    // This function is used to save a mood
+    function setMood(string memory _mood) public {
+        mood = _mood;
+    }
 
-//     function claimWill(address _owner, uint256 _index) public {
-//         require(_index < wills[_owner].length, "Invalid will index");
+    // This function is used to read the mood
+    function getMood() public view returns (string memory) {
+        return mood;
+    }
+}
 
-//         Will storage userWill = wills[_owner][_index];
-//         require(msg.sender == userWill.recipient, "Only recipient can claim");
-//         require(!userWill.claimed, "Already claimed");
-//         require(userWill.amount > 0, "No funds to claim");
 
-//         userWill.claimed = true;
-//         payable(userWill.recipient).transfer(userWill.amount);
-//     }
-
-//     function getMyWillsCount() public view returns (uint256) {
-//         return wills[msg.sender].length;
-//     }
-
-//     function getWill(address _owner, uint256 _index) public view returns (address recipient, uint256 amount, bool claimed) {
-//         require(_index < wills[_owner].length, "Invalid will index");
-//         Will memory userWill = wills[_owner][_index];
-//         return (userWill.recipient, userWill.amount, userWill.claimed);
-//     }
-
-//     function getContractBalance() public view returns (uint256) {
-//         return address(this).balance;
-//     }
-// }
